e05747d83ce00b7e7a6ac97a2413c62d07a038fe
Dzmitry Malyshau <kvarkus@gmail.com>
2016-04-05 23:51:53 -0400

    Ported the basic example

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Features:
 - Automatic execution of the systems in parallel. Follows Rust ownership rules. Depends on the order the systems are started.
 - Component storage is abstract behind the trait. One can use vectors, hashmaps, trees, or whatever else.
 - New components can be registered at any point from user modules. They don't have to be POD.
-- No virtual calls, low overhead.
+- No unsafe code! No virtual calls, low overhead.
 
 Example system:
 ```rust
-schedule.run1w1r(|b: &mut bool, a: &i8| {
-    *b = *a > 0;
+schedule.run1w1r(|p: &mut Position, s: &Speed| {
+    *p += *s;
 });
 ```
---
+++ examples/basic.rs
@@ -0,0 +1,40 @@
+extern crate parsec;
+
+#[derive(Clone, Debug)]
+struct CompInt(i8);
+impl parsec::Component for CompInt {
+    type Storage = parsec::VecStorage<CompInt>;
+}
+#[derive(Clone, Debug)]
+struct CompBool(bool);
+impl parsec::Component for CompBool {
+    type Storage = parsec::VecStorage<CompBool>;
+}
+
+fn main() {
+    let mut w = parsec::World::new();
+    w.register::<CompInt>();
+    w.register::<CompBool>();
+    let mut scheduler = parsec::Scheduler::new(w, 4);
+    scheduler.add_entity().with(CompInt(4)).with(CompBool(false)).build();
+    scheduler.add_entity().with(CompInt(-1)).with(CompBool(false)).build();
+
+    scheduler.run1w1r(|b: &mut CompBool, a: &CompInt| {
+        b.0 = a.0 > 0;
+    });
+    scheduler.run(|warg| {
+        let mut sa = warg.write::<CompInt>();
+        let sb = warg.read::<CompBool>();
+        //println!("{:?} {:?}", &*sa, &*sb);
+        for &ent in warg.entities().iter() {
+            use parsec::Storage;
+            if let (Some(a), Some(b)) = (sa.get_mut(ent), sb.get(ent)) {
+                a.0 = if b.0 {2} else {0};
+            }
+        }
+    });
+    scheduler.run0w2r(|a: &CompInt, b: &CompBool| {
+        println!("Entity {} {}", a.0, b.0);
+    });
+    scheduler.wait();
+}
