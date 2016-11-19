815773ce518a5c48159bd6ac4e8932e2c2283fe8
Harry Stern <hcs@meow.sh>
2016-11-19 00:22:29 -0500

    Wrap message example in mod cfg'd on parallel
    - Build failed without parallel feature
    - We wrap the structs in a mod rather than cfg'ing each item

--- examples/messages.rs
+++ examples/messages.rs
@@ -1,72 +1,83 @@
 extern crate specs;
-use specs::{RunArg, System, MessageQueue};
 
-#[derive(Clone, Debug)]
-enum Message {
-    Hello(String),
-    Goodbye(String),
-}
+#[cfg(feature="parallel")]
+mod msg_example {
+    use specs::{RunArg, System, MessageQueue};
 
-struct HelloSystem {}
-impl System<Message, ()> for HelloSystem {
-    fn run(&mut self, arg: RunArg, msg: MessageQueue<Message>, _: ()) {
-        let _ = arg.fetch(|_|{});
-        msg.send(Message::Hello("hey".to_owned()));
+    #[derive(Clone, Debug)]
+    pub enum Message {
+        Hello(String),
+        Goodbye(String),
     }
 
-    fn handle_message(&mut self, msg: &Message) {
-        use Message::*;
-        match *msg {
-            Hello(_) => println!("Got a hello!"),
-            Goodbye(_) => (),
+    pub struct HelloSystem {}
+    impl System<Message, ()> for HelloSystem {
+        fn run(&mut self, arg: RunArg, msg: MessageQueue<Message>, _: ()) {
+            let _ = arg.fetch(|_|{});
+            msg.send(Message::Hello("hey".to_owned()));
+        }
+
+        fn handle_message(&mut self, msg: &Message) {
+            use self::Message::*;
+            match *msg {
+                Hello(_) => println!("Got a hello!"),
+                Goodbye(_) => (),
+            }
         }
     }
-}
 
-struct GoodbyeSystem {}
-impl System<Message, ()> for GoodbyeSystem {
-    fn run(&mut self, arg: RunArg, msg: MessageQueue<Message>, _: ()) {
-        let _ = arg.fetch(|_|{});
-        msg.send(Message::Goodbye("bye".to_owned()));
-    }
+    pub struct GoodbyeSystem {}
+    impl System<Message, ()> for GoodbyeSystem {
+        fn run(&mut self, arg: RunArg, msg: MessageQueue<Message>, _: ()) {
+            let _ = arg.fetch(|_|{});
+            msg.send(Message::Goodbye("bye".to_owned()));
+        }
 
-    fn handle_message(&mut self, msg: &Message) {
-        use Message::*;
-        match *msg {
-            Hello(_) => (),
-            Goodbye(_) => println!("Got a goodbye!"),
+        fn handle_message(&mut self, msg: &Message) {
+            use self::Message::*;
+            match *msg {
+                Hello(_) => (),
+                Goodbye(_) => println!("Got a goodbye!"),
+            }
         }
     }
-}
 
-struct GreetzCounter {
-    hello: u32,
-    goodbye: u32,
-}
-
-impl System<Message, ()> for GreetzCounter {
-    fn run(&mut self, arg: RunArg, _: MessageQueue<Message>, _: ()) {
-        let _ = arg.fetch(|_|{});
-        println!("I have seen {} hellos.", self.hello);
-        println!("I have seen {} goodbyes.", self.goodbye);
+    pub struct GreetzCounter {
+        pub hello: u32,
+        pub goodbye: u32,
     }
 
-    fn handle_message(&mut self, msg: &Message) {
-        use Message::*;
-        match *msg {
-            Hello(_) => self.hello += 1,
-            Goodbye(_) => self.goodbye += 1,
+    impl System<Message, ()> for GreetzCounter {
+        fn run(&mut self, arg: RunArg, _: MessageQueue<Message>, _: ()) {
+            let _ = arg.fetch(|_|{});
+            println!("I have seen {} hellos.", self.hello);
+            println!("I have seen {} goodbyes.", self.goodbye);
+        }
+
+        fn handle_message(&mut self, msg: &Message) {
+            use self::Message::*;
+            match *msg {
+                Hello(_) => self.hello += 1,
+                Goodbye(_) => self.goodbye += 1,
+            }
         }
     }
+
 }
+
+#[cfg(not(feature="parallel"))]
+fn main() {
+}
+
+#[cfg(feature="parallel")]
 fn main() {
     let mut planner = {
         let w = specs::World::new();
-        specs::Planner::<Message,()>::new(w, 4)
+        specs::Planner::<msg_example::Message,()>::new(w, 4)
     };
-    let h = HelloSystem {};
-    let g = GoodbyeSystem {};
-    let ctr = GreetzCounter { hello: 0, goodbye: 0 };
+    let h = msg_example::HelloSystem {};
+    let g = msg_example::GoodbyeSystem {};
+    let ctr = msg_example::GreetzCounter { hello: 0, goodbye: 0 };
 
     planner.add_system(h, "hello", 1);
     planner.add_system(g, "goodbye", 2);
