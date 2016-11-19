c9aeec257fdf98888f89d8d8c1228bbd25c1658e
Harry Stern <hcs@meow.sh>
2016-11-19 03:15:27 -0500

    Add documentation for message_out member

--- src/planner.rs
+++ src/planner.rs
@@ -125,6 +125,7 @@ pub struct Planner<M, C> {
     wait_count: usize,
     chan_out: mpsc::Sender<SystemInfo<M, C>>,
     chan_in: mpsc::Receiver<SystemInfo<M, C>>,
+    /// Message queue for sending messages to systems in the planner.
     pub message_out: MessageQueue<M>,
     message_in: mpsc::Receiver<M>,
     threader: ThreadPool,
