1298d3f5d26c91a2f6251abf4648128dd97bde7a
torkleyy <thomas-schaller2000@gmx.de>
2017-05-25 05:49:50 +0200

    Organize modules

--- src/lib.rs
+++ src/lib.rs
@@ -19,19 +19,70 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
+pub use shred::{AsyncDispatcher, Dispatcher, DispatcherBuilder, Resource, System};
+
 pub use entity::{Component, Entity, Entities};
 pub use join::{Join, JoinIter};
 pub use world::World;
-pub use storage::{CheckStorage, InsertResult, ReadStorage, Storage, UnprotectedStorage,
-                  WriteStorage};
+pub use storage::{CheckStorage, InsertResult, UnprotectedStorage};
 
 #[cfg(feature = "serialize")]
 pub use storage::{MergeError, PackedData};
+
+/// Reexports for types implementing `SystemData`.
+///
+/// # Examples
+///
+/// These can be used in a `System` implementation
+///
+/// ```
+/// # use specs::Resource;
+/// use specs::System;
+/// use specs::data::{Entities, FetchMut, WriteStorage}:
+///
+/// # #[derive(Debug)] struct MyComp;
+/// # #[derive(Debug)] struct MyRes; impl Resource for MyRes {}
+///
+/// struct MySys;
+///
+/// impl<'a, C> System<'a, C> for MySys {
+///     type SystemData = (Entities<'a>, FetchMut<'a, MyRes>, WriteStorage<'a, MyComp>);
+///
+///     fn work(&mut self, data: Self::SystemData, _: C) {
+///         // ..
+///
+///         # let _ = data;
+///     }
+/// }
+/// ```
+pub mod data {
+    pub use shred::{Fetch, FetchId, FetchIdMut, FetchMut, SystemData};
+
+    pub use storage::{ReadStorage, Storage, WriteStorage};
+
+    /// A wrapper for a fetched `Entities` resource.
+    /// Note that this is just `Fetch<Entities>`, so
+    /// you can easily use it in your system:
+    ///
+    /// ```
+    /// type SystemData = (Entities<'a>, ...);
+    /// ```
+    pub type Entities<'a> = Fetch<'a, ::entity::Entities>;
+}
 
 /// Entity related types.
 pub mod entity {
     pub use world::{Component, CreateIter, CreateIterAtomic, Entity, Entities, EntityBuilder,
                     Generation};
+}
+
+/// Reexports for very common types.
+mod prelude {
+    pub use {Component, Dispatcher, DispatcherBuilder, Resource, System, World};
+
+    pub use data::{Entities, Fetch, FetchMut, ReadStorage, WriteStorage};
+    pub use join::Join;
+    pub use storages::{DenseVecStorage, HashMapStorage, VecStorage};
 }
 
 pub use storage::storages;
