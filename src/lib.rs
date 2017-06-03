c4418cb51c06cd9215fbe9b995d9986848952448
torkleyy <thomas-schaller2000@gmx.de>
2017-06-03 10:37:54 +0200

    Improve crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,124 @@
 //! This library provides an ECS variant designed for parallel execution
 //! and convenient usage. It is highly flexible when it comes to actual
 //! component data and the way it is stored and accessed.
+//!
+//! ## High-level overview
+//!
+//! One could basically split this library up into two parts:
+//! The data part and the execution part.
+//!
+//! ### The data
+//!
+//! `World` is where component storages, resources and entities are stored.
+//! See the docs of [`World`] for more.
+//!
+//! [`World`]: ./world/struct.World.html
+//!
+//! Components can be easily implemented like this:
+//!
+//! ```rust
+//! use specs::prelude::*;
+//!
+//! struct MyComp;
+//!
+//! impl Component for MyComp {
+//!     type Storage = VecStorage<MyComp>;
+//! }
+//! ```
+//!
+//! You can choose different storages according to your needs.
+//!
+//! ### System execution
+//!
+//! One part of this is `System` and `Dispatcher`. Both types
+//! are provided by a library called `shred`.
+//!
+//! The `Dispatcher` can be seen as an optional part here;
+//! it allows dispatching the systems in parallel, given a list
+//! of systems and their dependencies on other systems.
+//!
+//! `System`s are traits with a `run()` method and an associated
+//! `SystemData`, allowing type-safe aspects (knowledge about the
+//! reads / writes of the systems).
+//!
+//! To access components ergonomically, Specs provides a `Join` trait
+//! which joins component storages together in an efficient manner.
+//!
+//! ## Examples
+//!
+//! This is a basic example of using Specs:
+//!
+//! ```rust
+//! extern crate specs;
+//!
+//! use specs::prelude::*;
+//!
+//! // A component contains data
+//! // which is associated with an entity.
+//! struct Vel(f32);
+//! struct Pos(f32);
+//!
+//! impl Component for Vel {
+//!     type Storage = VecStorage<Vel>;
+//! }
+//!
+//! impl Component for Pos {
+//!     type Storage = VecStorage<Pos>;
+//! }
+//!
+//! struct SysA;
+//!
+//! impl<'a> System<'a> for SysA {
+//!     // These are the resources required for execution.
+//!     // You can also define a struct and `#[derive(SystemData)]`,
+//!     // see the `full` example.
+//!     type SystemData = (WriteStorage<'a, Pos>, ReadStorage<'a, Vel>);
+//!
+//!     fn run(&mut self, data: Self::SystemData) {
+//!         // The `.join()` combines multiple components,
+//!         // so we only access those entities which have
+//!         // both of them.
+//!
+//!         let (mut pos, vel) = data;
+//!
+//!         for (pos, vel) in (&mut pos, &vel).join() {
+//!             pos.0 += vel.0;
+//!         }
+//!     }
+//! }
+//!
+//! fn main() {
+//!     // The `World` is our
+//!     // container for components
+//!     // and other resources.
+//!
+//!     let mut world = World::new();
+//!     world.register::<Pos>();
+//!     world.register::<Vel>();
+//!
+//!     // An entity may or may not contain some component.
+//!
+//!     world.create_entity().with(Vel(2.0)).with(Pos(0.0)).build();
+//!     world.create_entity().with(Vel(4.0)).with(Pos(1.6)).build();
+//!     world.create_entity().with(Vel(1.5)).with(Pos(5.4)).build();
+//!
+//!     // This entity does not have `Vel`, so it won't be dispatched.
+//!     world.create_entity().with(Pos(2.0)).build();
+//!
+//!     // This builds a dispatcher.
+//!     // The third parameter of `add` specifies
+//!     // logical dependencies on other systems.
+//!     // Since we only have one, we don't depend on anything.
+//!     // See the `full` example for dependencies.
+//!     let mut dispatcher = DispatcherBuilder::new().add(SysA, "sys_a", &[]).build();
+//!
+//!     // This dispatches all the systems in parallel (but blocking).
+//!     dispatcher.dispatch(&mut world.res);
+//! }
+//! ```
+//!
+//! See the repository's examples directory for more examples.
+//!
 
 extern crate atom;
 extern crate fnv;
