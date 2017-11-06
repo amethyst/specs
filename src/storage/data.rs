32642cafaeede0e28f8483439f9941d1d783483e
torkleyy <torkleyy@gmail.com>
2017-11-06 17:34:21 +0100

    Add docs to ReadStorage and WriteStorage

--- src/storage/data.rs
+++ src/storage/data.rs
@@ -4,6 +4,96 @@ use {Component, EntitiesRes, Storage};
 use storage::MaskedStorage;
 
 /// A storage with read access.
+///
+/// This is just a typedef for a fetched component storage.
+///
+/// The main functionality it provides is listed in the following,
+/// however make sure to also check out the documentation for the
+/// respective methods on `Storage`.
+///
+/// ## Joining storages
+///
+/// `&ReadStorage` implements `Join`, which allows to do
+/// something like this:
+///
+/// ```
+/// # use specs::*;
+/// #
+/// # struct Pos; impl Component for Pos { type Storage = VecStorage<Self>; }
+/// # struct Vel; impl Component for Vel { type Storage = VecStorage<Self>; }
+/// #
+/// # let mut world = World::new(); world.register::<Pos>(); world.register::<Vel>();
+/// # let pos_storage = world.read::<Pos>();
+/// # let vel_storage = world.read::<Vel>();
+/// (&pos_storage, &vel_storage).join()
+/// # ;
+/// ```
+///
+/// This joins the position and the velocity storage, thus only components
+/// that exist in both storages are iterated over.
+///
+/// ## Retrieving single components
+///
+/// If you have an entity (for example because you stored it before
+/// or because you're joining over `Entities`), you can get a single
+/// component by calling `Storage::get`:
+///
+/// ```
+/// # use specs::*;
+/// # #[derive(Debug, PartialEq)]
+/// # struct Pos; impl Component for Pos { type Storage = VecStorage<Self>; }
+/// # #[derive(Debug, PartialEq)]
+/// # struct Vel; impl Component for Vel { type Storage = VecStorage<Self>; }
+/// #
+/// # let mut world = World::new(); world.register::<Pos>(); world.register::<Vel>();
+/// let entity1 = world.create_entity()
+///     .with(Pos)
+///     .build();
+/// let entity2 = world.create_entity()
+///     .with(Vel)
+///     .build();
+///
+/// # let pos_storage = world.read::<Pos>();
+/// # let vel_storage = world.read::<Vel>();
+/// assert_eq!(pos_storage.get(entity1), Some(&Pos));
+/// assert_eq!(pos_storage.get(entity2), None);
+///
+/// assert_eq!(vel_storage.get(entity1), None);
+/// assert_eq!(vel_storage.get(entity2), Some(&Vel));
+/// ```
+///
+/// ## Usage as `SystemData`
+///
+/// `ReadStorage` implements `SystemData` which allows you to
+/// fetch it inside a system by simply adding it to the tuple:
+///
+/// ```
+/// # use specs::*;
+/// #[derive(Debug)]
+/// struct Pos {
+///     x: f32,
+///     y: f32,
+/// }
+///
+/// impl Component for Pos {
+///     type Storage = VecStorage<Self>;
+/// }
+///
+/// struct Sys;
+///
+/// impl<'a> System<'a> for Sys {
+///     type SystemData = (Entities<'a>, ReadStorage<'a, Pos>);
+///
+///     fn run(&mut self, (ent, pos): Self::SystemData) {
+///         for (ent, pos) in (&*ent, &pos).join() {
+///             println!("Entitiy with id {} has a position of {:?}", ent.id(), pos);
+///         }
+///     }
+/// }
+/// ```
+///
+/// These operations can't mutate anything; if you want to do
+/// insertions or modify components, you need to use `WriteStorage`.
 pub type ReadStorage<'a, T> = Storage<'a, T, Fetch<'a, MaskedStorage<T>>>;
 
 impl<'a, T> SystemData<'a> for ReadStorage<'a, T>
@@ -27,6 +117,54 @@ where
 }
 
 /// A storage with read and write access.
+///
+/// In addition to `ReadStorage`, a storage with mutable access allows to:
+///
+/// ## Retrieve components mutably
+///
+/// This works just like `Storage::get`, but returns a mutable reference:
+///
+/// ```
+/// # use specs::*;
+/// # #[derive(Debug, PartialEq)]
+/// # struct Pos(f32); impl Component for Pos { type Storage = VecStorage<Self>; }
+/// #
+/// # let mut world = World::new(); world.register::<Pos>();
+/// let entity = world.create_entity()
+///     .with(Pos(2.0))
+///     .build();
+/// # let mut pos_storage = world.write::<Pos>();
+///
+/// assert_eq!(pos_storage.get_mut(entity), Some(&mut Pos(2.0)));
+/// if let Some(pos) = pos_storage.get_mut(entity) {
+///     *pos = Pos(4.5);
+/// }
+///
+/// assert_eq!(pos_storage.get(entity), Some(&Pos(4.5)));
+/// ```
+///
+/// ## Inserting and removing components
+///
+/// You can insert components using `Storage::insert` and remove them
+/// again with `Storage::remove`.
+///
+/// ```
+/// # use specs::*;
+/// # #[derive(Debug, PartialEq)]
+/// # struct Pos(f32); impl Component for Pos { type Storage = VecStorage<Self>; }
+/// #
+/// # let mut world = World::new(); world.register::<Pos>();
+/// let entity = world.create_entity()
+///     .with(Pos(0.1))
+///     .build();
+/// # let mut pos_storage = world.write::<Pos>();
+///
+/// let res = pos_storage.insert(entity, Pos(4.0));
+/// assert_eq!(res, InsertResult::Updated(Pos(0.1)));
+/// ```
+///
+/// There's also an Entry-API similar to the one provided by `std`.
+///
 pub type WriteStorage<'a, T> = Storage<'a, T, FetchMut<'a, MaskedStorage<T>>>;
 
 impl<'a, T> SystemData<'a> for WriteStorage<'a, T>
