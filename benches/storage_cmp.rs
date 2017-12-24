8eb119c427e948f5dbc47469795f38d596cf142b
torkleyy <torkleyy@gmail.com>
2017-12-24 13:12:37 +0100

    Use criterion for benchmarks
    Finish benchmarking transition to criterion.rs
    
    Put real_blackbox behind nightly, ensure 1.19
    
    Fix TODOs
    
    Try with 1.20
    
    Try 1.21
    
    Give up

---
+++ benches/storage_cmp.rs
@@ -0,0 +1,257 @@
+use criterion::{Bencher, Criterion};
+use specs::prelude::*;
+use specs::storage;
+
+use super::black_box;
+
+fn storage_insert<C>(b: &mut Bencher, num: usize)
+where
+    C: Component + Default,
+    C::Storage: Default,
+{
+    b.iter_with_setup(
+        || {
+            let mut world = World::new();
+
+            world.register::<C>();
+
+            world
+        },
+        |world| {
+            let entities = world.entities();
+            let mut storage = world.write::<C>();
+
+            for e in entities.create_iter().take(num) {
+                storage.insert(e, C::default());
+            }
+        },
+    )
+}
+
+fn storage_remove<C>(b: &mut Bencher, num: usize)
+where
+    C: Component + Default,
+    C::Storage: Default,
+{
+    b.iter_with_setup(
+        || {
+            let mut world = World::new();
+
+            world.register::<C>();
+
+            {
+                let entities = world.entities();
+                let mut storage = world.write::<C>();
+
+                for e in entities.create_iter().take(num) {
+                    storage.insert(e, C::default());
+                }
+            }
+
+            world
+        },
+        |world| {
+            let entities = world.entities();
+            let mut storage = world.write::<C>();
+
+            for e in (&*entities).join() {
+                storage.remove(e);
+            }
+        },
+    )
+}
+
+fn storage_get<C>(b: &mut Bencher, num: usize)
+where
+    C: Component + Default,
+    C::Storage: Default,
+{
+    b.iter_with_setup(
+        || {
+            let mut world = World::new();
+
+            world.register::<C>();
+
+            {
+                let entities = world.entities();
+                let mut storage = world.write::<C>();
+
+                for e in entities.create_iter().take(num) {
+                    storage.insert(e, C::default());
+                }
+            }
+
+            world
+        },
+        |world| {
+            let entities = world.entities();
+            let storage = world.read::<C>();
+
+            for e in (&*entities).join() {
+                black_box(storage.get(e));
+            }
+        },
+    )
+}
+
+macro_rules! decl_comp {
+    ($bytes:expr, $store:ident) => {
+        #[derive(Default)]
+        struct Comp {
+            _x: [u8; $bytes],
+        }
+
+        impl Component for Comp {
+            type Storage = storage::$store<Self>;
+        }
+    };
+}
+
+macro_rules! insert {
+    ($b:ident, $num:expr, $bytes:expr, $store:ident) => {
+        {
+            decl_comp!($bytes, $store);
+
+            storage_insert::<Comp>($b, $num)
+        }
+    };
+}
+
+macro_rules! remove {
+    ($b:ident, $num:expr, $bytes:expr, $store:ident) => {
+        {
+            decl_comp!($bytes, $store);
+
+            storage_remove::<Comp>($b, $num)
+        }
+    };
+}
+
+macro_rules! get {
+    ($b:ident, $num:expr, $bytes:expr, $store:ident) => {
+        {
+            decl_comp!($bytes, $store);
+
+            storage_get::<Comp>($b, $num)
+        }
+    };
+}
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+fn insert_benches(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "insert 1b/dense",
+        |b, &&i| insert!(b, i, 1, DenseVecStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "insert 1b/btree",
+        |b, &&i| insert!(b, i, 1, BTreeStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "insert 1b/hash",
+        |b, &&i| insert!(b, i, 1, HashMapStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "insert 1b/vec",
+        |b, &&i| insert!(b, i, 1, VecStorage),
+        &[1, 16, 64, 256, 1024],
+    );
+
+    c.bench_function_over_inputs(
+        "insert 32b/dense",
+        |b, &&i| insert!(b, i, 32, DenseVecStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "insert 32b/btree",
+        |b, &&i| insert!(b, i, 32, BTreeStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "insert 32b/hash",
+        |b, &&i| insert!(b, i, 32, HashMapStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "insert 32b/vec",
+        |b, &&i| insert!(b, i, 32, VecStorage),
+        &[1, 16, 64, 256, 1024],
+    );
+}
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+fn remove_benches(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "remove 1b/dense",
+        |b, &&i| remove!(b, i, 1, DenseVecStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "remove 1b/btree",
+        |b, &&i| remove!(b, i, 1, BTreeStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "remove 1b/hash",
+        |b, &&i| remove!(b, i, 1, HashMapStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "remove 1b/vec",
+        |b, &&i| remove!(b, i, 1, VecStorage),
+        &[1, 16, 64, 256, 1024],
+    );
+
+    c.bench_function_over_inputs(
+        "remove 32b/dense",
+        |b, &&i| remove!(b, i, 32, DenseVecStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "remove 32b/btree",
+        |b, &&i| remove!(b, i, 32, BTreeStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "remove 32b/hash",
+        |b, &&i| remove!(b, i, 32, HashMapStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "remove 32b/vec",
+        |b, &&i| remove!(b, i, 32, VecStorage),
+        &[1, 16, 64, 256, 1024],
+    );
+}
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+fn get_benches(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "get 1b/dense",
+        |b, &&i| remove!(b, i, 1, DenseVecStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "get 1b/btree",
+        |b, &&i| remove!(b, i, 1, BTreeStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "get 1b/hash",
+        |b, &&i| remove!(b, i, 1, HashMapStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "get 1b/vec",
+        |b, &&i| remove!(b, i, 1, VecStorage),
+        &[1, 16, 64, 256, 1024],
+    );
+
+    c.bench_function_over_inputs(
+        "get 32b/dense",
+        |b, &&i| get!(b, i, 32, DenseVecStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "get 32b/btree",
+        |b, &&i| get!(b, i, 32, BTreeStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "get 32b/hash",
+        |b, &&i| get!(b, i, 32, HashMapStorage),
+        &[1, 16, 64, 256, 1024],
+    ).bench_function_over_inputs(
+        "get 32b/vec",
+        |b, &&i| get!(b, i, 32, VecStorage),
+        &[1, 16, 64, 256, 1024],
+    );
+}
+
+criterion_group!(benches_storages, insert_benches, remove_benches, get_benches);
---
+++ benches/storage_sparse.rs
@@ -0,0 +1,167 @@
+use criterion::{Bencher, Criterion};
+
+use super::black_box;
+
+macro_rules! setup {
+    ($num:expr => [ $( $comp:ty ),* ] ) => {
+        pub fn setup(filter: bool, insert: bool, sparsity: u32) -> (World, Vec<Entity>) {
+            let mut w = World::new();
+            $(
+                w.register::<$comp>();
+            )*
+
+            let eids: Vec<_> = (0..$num)
+                .flat_map(|i| {
+                    let mut builder = w.create_entity();
+                    if insert {
+                        if i % sparsity == 0 {
+                        $(
+                            builder = builder.with::<$comp>(<$comp>::default());
+                        )*
+                        }
+                    }
+
+                    if !filter || i % sparsity == 0 {
+                        Some(builder.build())
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+
+            (w, eids)
+        }
+    }
+}
+
+macro_rules! gap {
+    ( $storage:ident, $name:ident => $sparsity:expr ) => {
+        mod $name {
+            use super::{CompInt, CompBool, setup};
+            use super::super::{Bencher, Criterion, black_box};
+
+            fn insert(bencher: &mut Bencher) {
+                let (world, entities) = setup(true, false, $sparsity);
+                let mut ints = world.write::<CompInt>();
+                let mut bools = world.write::<CompBool>();
+
+                bencher.iter(move || {
+                    for &entity in &entities {
+                        ints.insert(entity, CompInt::default());
+                        bools.insert(entity, CompBool::default());
+                    }
+                });
+            }
+
+            fn remove(bencher: &mut Bencher) {
+                let (world, entities) = setup(true, true, $sparsity);
+                let mut ints = world.write::<CompInt>();
+                let mut bools = world.write::<CompBool>();
+
+                bencher.iter(move || {
+                    for &entity in &entities {
+                        ints.remove(entity);
+                        bools.remove(entity);
+                    }
+                });
+            }
+
+            fn get(bencher: &mut Bencher) {
+                let (world, entities) = setup(false, true, $sparsity);
+                let ints = world.read::<CompInt>();
+                let bools = world.read::<CompBool>();
+
+                bencher.iter(move || {
+                    for &entity in &entities {
+                        black_box(ints.get(entity));
+                        black_box(bools.get(entity));
+                    }
+                });
+            }
+
+            pub fn benches(c: &mut Criterion) {
+                c.bench_function(
+                    &format!("sparse insert {}/{}", $sparsity, stringify!($storage)),
+                    |b| insert(b),
+                ).bench_function(
+                    &format!("sparse remove {}/{}", $sparsity, stringify!($storage)),
+                    |b| remove(b),
+                ).bench_function(
+                    &format!("sparse get {}/{}", $sparsity, stringify!($storage)),
+                    |b| get(b),
+                );
+            }
+        }
+    }
+}
+
+macro_rules! tests {
+    ($mod:ident => $storage:ident) => {
+        mod $mod {
+            use criterion::Criterion;
+            use specs::prelude::*;
+
+            pub static NUM: u32 = 100_000;
+
+            pub struct CompInt(u32);
+            pub struct CompBool(bool);
+
+            impl Default for CompInt {
+                fn default() -> Self {
+                    CompInt(0)
+                }
+            }
+
+            impl Default for CompBool {
+                fn default() -> Self {
+                    CompBool(true)
+                }
+            }
+
+            impl Component for CompInt {
+                type Storage = ::specs::storage::$storage<Self>;
+            }
+            impl Component for CompBool {
+                type Storage = ::specs::storage::$storage<Self>;
+            }
+
+            setup!(NUM => [ CompInt, CompBool ]);
+
+            gap!($storage, sparse_1 => 1);
+            gap!($storage, sparse_2 => 2);
+            gap!($storage, sparse_4 => 4);
+            gap!($storage, sparse_8 => 8);
+            gap!($storage, sparse_128 => 128);
+            gap!($storage, sparse_256 => 256);
+            gap!($storage, sparse_512 => 512);
+            gap!($storage, sparse_1024 => 1024);
+            gap!($storage, sparse_10000 => 10_000);
+            gap!($storage, sparse_50000 => 50_000);
+
+            group!(benches,
+                sparse_1::benches,
+                sparse_2::benches,
+                sparse_4::benches,
+                sparse_8::benches,
+                sparse_128::benches,
+                sparse_256::benches,
+                sparse_512::benches,
+                sparse_1024::benches,
+                sparse_10000::benches,
+                sparse_50000::benches
+            );
+        }
+    }
+}
+
+tests!(vec_storage => VecStorage);
+tests!(dense_vec_storage => DenseVecStorage);
+tests!(hashmap_storage => HashMapStorage);
+tests!(btree_storage => BTreeStorage);
+
+criterion_group!(benches_sparse,
+    vec_storage::benches,
+    dense_vec_storage::benches,
+    hashmap_storage::benches,
+    btree_storage::benches
+);
