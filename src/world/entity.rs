7ddcde871e911ff3678d7beb4656fc02be203e1c
Andrew Hickman <andrew.hickman1@sky.com>
2018-09-21 18:27:50 +0100

    Revert "Remove unnecessary mappings"
    This reverts commit ab43ce19b0d334b893a99e33102d50c31f8a4678.

--- src/world/entity.rs
+++ src/world/entity.rs
@@ -140,6 +140,7 @@ impl Allocator {
         self.raised.add_atomic(id);
         let gen = self
             .generation(id)
+            .map(|gen| if gen.is_alive() { gen } else { gen.raised() })
             .unwrap_or(Generation::ONE);
         Entity(id, gen)
     }
@@ -320,6 +321,7 @@ impl<'a> Join for &'a EntitiesRes {
         let gen = v
             .alloc
             .generation(idx)
+            .map(|gen| if gen.is_alive() { gen } else { gen.raised() })
             .unwrap_or(Generation::ONE);
         Entity(idx, gen)
     }
