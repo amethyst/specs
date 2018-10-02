5dca58290bfe2b3d131c0c7587c60efb9a2edd0a
Aceeri <conmcclusk@gmail.com>
2018-10-02 15:20:58 -0700

    Copy for ComponentEvent, example for old usage

--- examples/track.rs
+++ examples/track.rs
@@ -13,6 +13,7 @@ impl Component for TrackedComponent {
 #[derive(Default)]
 struct SysA {
     reader_id: Option<ReaderId<ComponentEvent>>,
+    events: Vec<ComponentEvent>,
     inserted: BitSet,
     modified: BitSet,
     removed: BitSet,
@@ -27,15 +28,36 @@ impl<'a> System<'a> for SysA {
     }
 
     fn run(&mut self, (entities, tracked): Self::SystemData) {
+        self.events  = tracked.channel().read(self.reader_id.as_mut().expect("ReaderId not found")).map(|e| *e).collect();
+
         self.modified.clear();
-        let events = tracked.channel().read(self.reader_id.as_mut().expect("ReaderId not found"));
-        self.modified.extend(events.filter_map(|event| match event {
+        self.modified.extend(self.events.iter().filter_map(|event| match event {
             ComponentEvent::Modified(index) => Some(index),
+            _ => None,
+        }));
+
+        self.inserted.clear();
+        self.inserted.extend(self.events.iter().filter_map(|event| match event {
+            ComponentEvent::Inserted(index) => Some(index),
+            _ => None,
+        }));
+
+        self.removed.clear();
+        self.removed.extend(self.events.iter().filter_map(|event| match event {
+            ComponentEvent::Removed(index) => Some(index),
             _ => None,
         }));
 
         for (entity, _tracked, _) in (&entities, &tracked, &self.modified).join() {
             println!("modified: {:?}", entity);
+        }
+
+        for (entity, _tracked, _) in (&entities, &tracked, &self.inserted).join() {
+            println!("inserted: {:?}", entity);
+        }
+            
+        for (entity, _tracked, _) in (&entities, &tracked, &self.removed).join() {
+            println!("removed: {:?}", entity);
         }
     }
 }
@@ -64,12 +86,19 @@ fn main() {
 
     dispatcher.setup(&mut world.res);
 
-    for _ in 0..10000 {
+    for _ in 0..50 {
         world.create_entity().with(TrackedComponent(0)).build();
     }
 
     dispatcher.dispatch(&mut world.res);
     world.maintain();
+
+    let entities = (&world.entities(), &world.read_storage::<TrackedComponent>()).join().map(|(e, _)| e).collect::<Vec<Entity>>();
+    world.delete_entities(&entities);
+
+    for _ in 0..50 {
+        world.create_entity().with(TrackedComponent(0)).build();
+    }
 
     dispatcher.dispatch(&mut world.res);
     world.maintain();
