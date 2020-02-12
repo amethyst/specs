b4e83ceda356631f480fc488e7c7f716bf82f903
Azriel Hoh <azriel91@gmail.com>
2020-02-12 16:35:19 +1300

    Use macro to generate feature gate.

--- src/world/lazy.rs
+++ src/world/lazy.rs
@@ -11,61 +11,49 @@ impl<T> Default for Queue<T> {
 }
 
 #[cfg(feature = "parallel")]
-mod traits_internal {
-    use crate::{Component, Entity, World};
-
-    pub trait LazyUpdateInternal: Send + Sync {
-        fn update(self: Box<Self>, world: &mut World);
-    }
-
-    pub trait FnLazyUpdate: FnOnce(&World) + Send + Sync + 'static {}
-    impl<F> FnLazyUpdate for F where F: FnOnce(&World) + Send + Sync + 'static {}
-
-    pub trait FnLazyUpdateMut: FnOnce(&mut World) + Send + Sync + 'static {}
-    impl<F> FnLazyUpdateMut for F where F: FnOnce(&mut World) + Send + Sync + 'static {}
-
-    pub trait ComponentBound: Component + Send + Sync {}
-    impl<C> ComponentBound for C where C: Component + Send + Sync {}
-
-    pub trait IntoIteratorBound<C>:
-        IntoIterator<Item = (Entity, C)> + Send + Sync + 'static
-    {
-    }
-    impl<I, C> IntoIteratorBound<C> for I
-    where
-        I: IntoIterator<Item = (Entity, C)> + Send + Sync + 'static,
-        C: ComponentBound,
-    {
-    }
+pub trait LazyUpdateInternal: Send + Sync {
+    fn update(self: Box<Self>, world: &mut World);
 }
 
 #[cfg(not(feature = "parallel"))]
-mod traits_internal {
-    use crate::{Component, Entity, World};
-
-    pub trait LazyUpdateInternal {
-        fn update(self: Box<Self>, world: &mut World);
-    }
-
-    pub trait FnLazyUpdate: FnOnce(&World) + 'static {}
-    impl<F> FnLazyUpdate for F where F: FnOnce(&World) + 'static {}
-
-    pub trait FnLazyUpdateMut: FnOnce(&mut World) + 'static {}
-    impl<F> FnLazyUpdateMut for F where F: FnOnce(&mut World) + 'static {}
-
-    pub trait ComponentBound: Component {}
-    impl<C> ComponentBound for C where C: Component {}
-
-    pub trait IntoIteratorBound<C>: IntoIterator<Item = (Entity, C)> + 'static {}
-    impl<C, I> IntoIteratorBound<C> for I
-    where
-        I: IntoIterator<Item = (Entity, C)> + 'static,
-        C: ComponentBound,
-    {
-    }
+pub trait LazyUpdateInternal {
+    fn update(self: Box<Self>, world: &mut World);
 }
 
-use traits_internal::*;
+macro_rules! parallel_feature {
+    (
+        $(
+            $(#[$attrs:meta])* $($words:ident)+<$($ty_params:ident),+> $args:tt $(-> $return_ty:ty)?
+            where
+                $($ty_param:ident:
+                    $bound_first:ident $(< $ty_type:ident = $ty_bound:tt >)? $(($($fn_bound:tt)*))?
+                    $(+ $bound:tt $($bound_2:ident)?)*,)+
+            $body:block
+        )+
+    ) =>
+    {
+        $(
+            $(#[$attrs])*
+            #[cfg(feature = "parallel")]
+            $($words)+<$($ty_params),+> $args $(-> $return_ty)?
+            where
+                $($ty_param:
+                    Send + Sync +
+                    $bound_first $(< $ty_type = $ty_bound >)? $(($($fn_bound)*))?
+                    $(+ $bound $($bound_2)?)*,)+
+            $body
+
+            $(#[$attrs])*
+            #[cfg(not(feature = "parallel"))]
+            $($words)+<$($ty_params),+> $args $(-> $return_ty)?
+            where
+                $($ty_param:
+                    $bound_first $(< $ty_type = $ty_bound >)? $(($($fn_bound)*))?
+                    $(+ $bound $($bound_2)?)*,)+
+            $body
+        )+
+    };
+}
 
 /// Like `EntityBuilder`, but inserts the component
 /// lazily, meaning on `maintain`.
@@ -80,26 +68,28 @@ pub struct LazyBuilder<'a> {
 }
 
 impl<'a> Builder for LazyBuilder<'a> {
-    /// Inserts a component using [LazyUpdate].
-    ///
-    /// If a component was already associated with the entity, it will
-    /// overwrite the previous component.
-    fn with<C>(self, component: C) -> Self
-    where
-        C: ComponentBound,
-    {
-        let entity = self.entity;
-        self.lazy.exec(move |world: &World| {
-            let mut storage: WriteStorage<C> = SystemData::fetch(world);
-            if storage.insert(entity, component).is_err() {
-                log::warn!(
-                    "Lazy insert of component failed because {:?} was dead.",
-                    entity
-                );
-            }
-        });
+    parallel_feature! {
+        /// Inserts a component using [LazyUpdate].
+        ///
+        /// If a component was already associated with the entity, it will
+        /// overwrite the previous component.
+        fn with<C>(self, component: C) -> Self
+        where
+            C: Component,
+        {
+            let entity = self.entity;
+            self.lazy.exec(move |world| {
+                let mut storage: WriteStorage<C> = SystemData::fetch(world);
+                if storage.insert(entity, component).is_err() {
+                    log::warn!(
+                        "Lazy insert of component failed because {:?} was dead.",
+                        entity
+                    );
+                }
+            });
 
-        self
+            self
+        }
     }
 
     /// Finishes the building and returns the built entity.
@@ -110,9 +100,20 @@ impl<'a> Builder for LazyBuilder<'a> {
     }
 }
 
+#[cfg(feature = "parallel")]
 impl<F> LazyUpdateInternal for F
 where
-    F: FnLazyUpdateMut,
+    F: FnOnce(&mut World) + Send + Sync + 'static,
+{
+    fn update(self: Box<Self>, world: &mut World) {
+        self(world);
+    }
+}
+
+#[cfg(not(feature = "parallel"))]
+impl<F> LazyUpdateInternal for F
+where
+    F: FnOnce(&mut World) + 'static,
 {
     fn update(self: Box<Self>, world: &mut World) {
         self(world);
@@ -145,6 +146,192 @@ impl Default for LazyUpdate {
 }
 
 impl LazyUpdate {
+    parallel_feature! {
+        /// Lazily inserts a component for an entity.
+        ///
+        /// ## Examples
+        ///
+        /// ```
+        /// # use specs::prelude::*;
+        /// #
+        /// struct Pos(f32, f32);
+        ///
+        /// impl Component for Pos {
+        ///     type Storage = VecStorage<Self>;
+        /// }
+        ///
+        /// struct InsertPos;
+        ///
+        /// impl<'a> System<'a> for InsertPos {
+        ///     type SystemData = (Entities<'a>, Read<'a, LazyUpdate>);
+        ///
+        ///     fn run(&mut self, (ent, lazy): Self::SystemData) {
+        ///         let a = ent.create();
+        ///         lazy.insert(a, Pos(1.0, 1.0));
+        ///     }
+        /// }
+        /// ```
+        pub fn insert<C>(&self, e: Entity, c: C)
+        where
+            C: Component,
+        {
+            self.exec(move |world| {
+                let mut storage: WriteStorage<C> = SystemData::fetch(world);
+                if storage.insert(e, c).is_err() {
+                    log::warn!("Lazy insert of component failed because {:?} was dead.", e);
+                }
+            });
+        }
+
+        /// Lazily inserts components for entities.
+        ///
+        /// ## Examples
+        ///
+        /// ```
+        /// # use specs::prelude::*;
+        /// #
+        /// struct Pos(f32, f32);
+        ///
+        /// impl Component for Pos {
+        ///     type Storage = VecStorage<Self>;
+        /// }
+        ///
+        /// struct InsertPos;
+        ///
+        /// impl<'a> System<'a> for InsertPos {
+        ///     type SystemData = (Entities<'a>, Read<'a, LazyUpdate>);
+        ///
+        ///     fn run(&mut self, (ent, lazy): Self::SystemData) {
+        ///         let a = ent.create();
+        ///         let b = ent.create();
+        ///
+        ///         lazy.insert_all(vec![(a, Pos(3.0, 1.0)), (b, Pos(0.0, 4.0))]);
+        ///     }
+        /// }
+        /// ```
+        pub fn insert_all<C, I>(&self, iter: I)
+        where
+            C: Component,
+            I: IntoIterator<Item = (Entity, C)> + 'static,
+        {
+            self.exec(move |world| {
+                let mut storage: WriteStorage<C> = SystemData::fetch(world);
+                for (e, c) in iter {
+                    if storage.insert(e, c).is_err() {
+                        log::warn!("Lazy insert of component failed because {:?} was dead.", e);
+                    }
+                }
+            });
+        }
+
+        /// Lazily removes a component.
+        ///
+        /// ## Examples
+        ///
+        /// ```
+        /// # use specs::prelude::*;
+        /// #
+        /// struct Pos;
+        ///
+        /// impl Component for Pos {
+        ///     type Storage = VecStorage<Self>;
+        /// }
+        ///
+        /// struct RemovePos;
+        ///
+        /// impl<'a> System<'a> for RemovePos {
+        ///     type SystemData = (Entities<'a>, Read<'a, LazyUpdate>);
+        ///
+        ///     fn run(&mut self, (ent, lazy): Self::SystemData) {
+        ///         for entity in ent.join() {
+        ///             lazy.remove::<Pos>(entity);
+        ///         }
+        ///     }
+        /// }
+        /// ```
+        pub fn remove<C>(&self, e: Entity)
+        where
+            C: Component,
+        {
+            self.exec(move |world| {
+                let mut storage: WriteStorage<C> = SystemData::fetch(world);
+                storage.remove(e);
+            });
+        }
+
+        /// Lazily executes a closure with world access.
+        ///
+        /// ## Examples
+        ///
+        /// ```
+        /// # use specs::prelude::*;
+        /// #
+        /// struct Pos;
+        ///
+        /// impl Component for Pos {
+        ///     type Storage = VecStorage<Self>;
+        /// }
+        ///
+        /// struct Execution;
+        ///
+        /// impl<'a> System<'a> for Execution {
+        ///     type SystemData = (Entities<'a>, Read<'a, LazyUpdate>);
+        ///
+        ///     fn run(&mut self, (ent, lazy): Self::SystemData) {
+        ///         for entity in ent.join() {
+        ///             lazy.exec(move |world| {
+        ///                 if world.is_alive(entity) {
+        ///                     println!("Entity {:?} is alive.", entity);
+        ///                 }
+        ///             });
+        ///         }
+        ///     }
+        /// }
+        /// ```
+        pub fn exec<F>(&self, f: F)
+        where
+            F: FnOnce(&mut World) + 'static,
+        {
+            self.queue
+                .as_ref()
+                .unwrap()
+                .0
+                .push(Box::new(|w: &mut World| f(w)));
+        }
+
+        /// Lazily executes a closure with mutable world access.
+        ///
+        /// This can be used to add a resource to the `World` from a system.
+        ///
+        /// ## Examples
+        ///
+        /// ```
+        /// # use specs::prelude::*;
+        /// #
+        ///
+        /// struct Sys;
+        ///
+        /// impl<'a> System<'a> for Sys {
+        ///     type SystemData = (Entities<'a>, Read<'a, LazyUpdate>);
+        ///
+        ///     fn run(&mut self, (ent, lazy): Self::SystemData) {
+        ///         for entity in ent.join() {
+        ///             lazy.exec_mut(move |world| {
+        ///                 // complete extermination!
+        ///                 world.delete_all();
+        ///             });
+        ///         }
+        ///     }
+        /// }
+        /// ```
+        pub fn exec_mut<F>(&self, f: F)
+        where
+            F: FnOnce(&mut World) + 'static,
+        {
+            self.queue.as_ref().unwrap().0.push(Box::new(f));
+        }
+    }
+
     /// Creates a new `LazyBuilder` which inserts components
     /// using `LazyUpdate`. This means that the components won't
     /// be available immediately, but only after a `maintain`
@@ -169,190 +356,6 @@ impl LazyUpdate {
         let entity = ent.create();
 
         LazyBuilder { entity, lazy: self }
-    }
-
-    /// Lazily inserts a component for an entity.
-    ///
-    /// ## Examples
-    ///
-    /// ```
-    /// # use specs::prelude::*;
-    /// #
-    /// struct Pos(f32, f32);
-    ///
-    /// impl Component for Pos {
-    ///     type Storage = VecStorage<Self>;
-    /// }
-    ///
-    /// struct InsertPos;
-    ///
-    /// impl<'a> System<'a> for InsertPos {
-    ///     type SystemData = (Entities<'a>, Read<'a, LazyUpdate>);
-    ///
-    ///     fn run(&mut self, (ent, lazy): Self::SystemData) {
-    ///         let a = ent.create();
-    ///         lazy.insert(a, Pos(1.0, 1.0));
-    ///     }
-    /// }
-    /// ```
-    pub fn insert<C>(&self, e: Entity, c: C)
-    where
-        C: ComponentBound,
-    {
-        self.exec(move |world: &World| {
-            let mut storage: WriteStorage<C> = SystemData::fetch(world);
-            if storage.insert(e, c).is_err() {
-                log::warn!("Lazy insert of component failed because {:?} was dead.", e);
-            }
-        });
-    }
-
-    /// Lazily inserts components for entities.
-    ///
-    /// ## Examples
-    ///
-    /// ```
-    /// # use specs::prelude::*;
-    /// #
-    /// struct Pos(f32, f32);
-    ///
-    /// impl Component for Pos {
-    ///     type Storage = VecStorage<Self>;
-    /// }
-    ///
-    /// struct InsertPos;
-    ///
-    /// impl<'a> System<'a> for InsertPos {
-    ///     type SystemData = (Entities<'a>, Read<'a, LazyUpdate>);
-    ///
-    ///     fn run(&mut self, (ent, lazy): Self::SystemData) {
-    ///         let a = ent.create();
-    ///         let b = ent.create();
-    ///
-    ///         lazy.insert_all(vec![(a, Pos(3.0, 1.0)), (b, Pos(0.0, 4.0))]);
-    ///     }
-    /// }
-    /// ```
-    pub fn insert_all<C, I>(&self, iter: I)
-    where
-        C: ComponentBound,
-        I: IntoIteratorBound<C>,
-    {
-        self.exec(move |world: &World| {
-            let mut storage: WriteStorage<C> = SystemData::fetch(world);
-            for (e, c) in iter {
-                if storage.insert(e, c).is_err() {
-                    log::warn!("Lazy insert of component failed because {:?} was dead.", e);
-                }
-            }
-        });
-    }
-
-    /// Lazily removes a component.
-    ///
-    /// ## Examples
-    ///
-    /// ```
-    /// # use specs::prelude::*;
-    /// #
-    /// struct Pos;
-    ///
-    /// impl Component for Pos {
-    ///     type Storage = VecStorage<Self>;
-    /// }
-    ///
-    /// struct RemovePos;
-    ///
-    /// impl<'a> System<'a> for RemovePos {
-    ///     type SystemData = (Entities<'a>, Read<'a, LazyUpdate>);
-    ///
-    ///     fn run(&mut self, (ent, lazy): Self::SystemData) {
-    ///         for entity in ent.join() {
-    ///             lazy.remove::<Pos>(entity);
-    ///         }
-    ///     }
-    /// }
-    /// ```
-    pub fn remove<C>(&self, e: Entity)
-    where
-        C: ComponentBound,
-    {
-        self.exec(move |world: &World| {
-            let mut storage: WriteStorage<C> = SystemData::fetch(world);
-            storage.remove(e);
-        });
-    }
-
-    /// Lazily executes a closure with world access.
-    ///
-    /// ## Examples
-    ///
-    /// ```
-    /// # use specs::prelude::*;
-    /// #
-    /// struct Pos;
-    ///
-    /// impl Component for Pos {
-    ///     type Storage = VecStorage<Self>;
-    /// }
-    ///
-    /// struct Execution;
-    ///
-    /// impl<'a> System<'a> for Execution {
-    ///     type SystemData = (Entities<'a>, Read<'a, LazyUpdate>);
-    ///
-    ///     fn run(&mut self, (ent, lazy): Self::SystemData) {
-    ///         for entity in ent.join() {
-    ///             lazy.exec(move |world| {
-    ///                 if world.is_alive(entity) {
-    ///                     println!("Entity {:?} is alive.", entity);
-    ///                 }
-    ///             });
-    ///         }
-    ///     }
-    /// }
-    /// ```
-    pub fn exec<F>(&self, f: F)
-    where
-        F: FnLazyUpdate,
-    {
-        self.queue
-            .as_ref()
-            .unwrap()
-            .0
-            .push(Box::new(|w: &mut World| f(w)));
-    }
-
-    /// Lazily executes a closure with mutable world access.
-    ///
-    /// This can be used to add a resource to the `World` from a system.
-    ///
-    /// ## Examples
-    ///
-    /// ```
-    /// # use specs::prelude::*;
-    /// #
-    ///
-    /// struct Sys;
-    ///
-    /// impl<'a> System<'a> for Sys {
-    ///     type SystemData = (Entities<'a>, Read<'a, LazyUpdate>);
-    ///
-    ///     fn run(&mut self, (ent, lazy): Self::SystemData) {
-    ///         for entity in ent.join() {
-    ///             lazy.exec_mut(move |world| {
-    ///                 // complete extermination!
-    ///                 world.delete_all();
-    ///             });
-    ///         }
-    ///     }
-    /// }
-    /// ```
-    pub fn exec_mut<F>(&self, f: F)
-    where
-        F: FnLazyUpdateMut,
-    {
-        self.queue.as_ref().unwrap().0.push(Box::new(f));
     }
 
     /// Allows to temporarily take the inner queue.
